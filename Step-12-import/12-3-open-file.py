02f0a8372f8a310f48a52687d65b5b23218d1209
Boxer-egg <tarsqing@gmail.com>
2023-06-25 23:17:32 +0800

    文件指针移到开头

--- Step-12-import/12-3-open-file.py
+++ Step-12-import/12-3-open-file.py
@@ -10,10 +10,14 @@ abc_path = os.path.join(script_dir, 'abc.txt')
 print(abc_path)
 
 file = open(abc_path, 'r')  # 打开并读取
+print('read和readlines的区别:', file.read(10))
+file.seek(0)   # 将文件指针移动到文件的开头
 print(file.readlines())  # 结果是个列表 加S就是读取所有。
+# 这个方法会读取文件中的所有行，然后将文件指针移动到文件的末尾。
 file.close()
 
 
+print('---')
 script_dir2 = os.path.dirname(os.path.abspath(__file__))
 print('再次打印，看看写的对不对：', script_dir2)
 ccc_path = os.path.join(script_dir2, 'ccc.txt')
@@ -24,4 +28,3 @@ file.close()
 
 file = open(ccc_path, 'a')
 file.write('\n使用‘a’,(追加一条）')
-
